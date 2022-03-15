1a14beec61d6199ac143f9f9bd7bbaca9675fb5b
garrettnorden20 <51890913+garrettnorden20@users.noreply.github.com>
2022-03-14 20:53:08 -0400

    Revert "update code"

--- ml.py
+++ ml.py
@@ -16,95 +16,9 @@ stats2_1 = pd.read_csv(prefix+"stats2_1.csv", low_memory=False)
 stats2_2 = pd.read_csv(prefix+"stats2_2.csv", low_memory=False)
 stats2_3 = pd.read_csv(prefix+"stats2_3.csv", low_memory=False)
 stats = pd.concat([stats1_1,stats1_2,stats1_3,stats1_4,stats2_1,stats2_2,stats2_3])
-# print(stats)
-
+print(stats)
 # merge into a single DataFrame
 a = pd.merge(participants, matches, left_on="matchid", right_on="id")
 allstats_orig = pd.merge(a, stats, left_on="matchid", right_on="id")
 allstats = allstats_orig.copy()
-
-# drop games that lasted less than 10 minutes
-allstats = allstats.loc[allstats["duration"] >= 10*60,:]
-# print(allstats)
-
-# Convert string-based categories to numeric values
-cat_cols = ["role", "position", "version", "platformid"]
-for c in cat_cols:
-    allstats[c] = allstats[c].astype('category')
-    allstats[c] = allstats[c].cat.codes
-allstats["wardsbought"] = allstats["wardsbought"].astype(np.int32)
-
-# print(allstats)
-X = allstats.drop(["win"], axis=1)
-y = allstats["win"]
-print(X)
-print(y)
-
-# convert all features we want to consider as rates
-rate_features = [
-    "kills", "deaths", "assists", "killingsprees", "doublekills",
-    "triplekills", "quadrakills", "pentakills", "legendarykills",
-    "totdmgdealt", "magicdmgdealt", "physicaldmgdealt", "truedmgdealt",
-    "totdmgtochamp", "magicdmgtochamp", "physdmgtochamp", "truedmgtochamp",
-    "totheal", "totunitshealed", "dmgtoobj", "timecc", "totdmgtaken",
-    "magicdmgtaken", "physdmgtaken", "truedmgtaken", "goldearned", "goldspent",
-    "totminionskilled", "neutralminionskilled", "ownjunglekills",
-    "enemyjunglekills", "totcctimedealt", "pinksbought", "wardsbought",
-    "wardsplaced", "wardskilled"
-]
-for feature_name in rate_features:
-    X[feature_name] /= X["duration"] / 60 # per minute rate
-
-# convert to fraction of game
-X["longesttimespentliving"] /= X["duration"]
-
-# define friendly names for the features
-full_names = {
-    "kills": "Kills per min.",
-    "deaths": "Deaths per min.",
-    "assists": "Assists per min.",
-    "killingsprees": "Killing sprees per min.",
-    "longesttimespentliving": "Longest time living as % of game",
-    "doublekills": "Double kills per min.",
-    "triplekills": "Triple kills per min.",
-    "quadrakills": "Quadra kills per min.",
-    "pentakills": "Penta kills per min.",
-    "legendarykills": "Legendary kills per min.",
-    "totdmgdealt": "Total damage dealt per min.",
-    "magicdmgdealt": "Magic damage dealt per min.",
-    "physicaldmgdealt": "Physical damage dealt per min.",
-    "truedmgdealt": "True damage dealt per min.",
-    "totdmgtochamp": "Total damage to champions per min.",
-    "magicdmgtochamp": "Magic damage to champions per min.",
-    "physdmgtochamp": "Physical damage to champions per min.",
-    "truedmgtochamp": "True damage to champions per min.",
-    "totheal": "Total healing per min.",
-    "totunitshealed": "Total units healed per min.",
-    "dmgtoobj": "Damage to objects per min.",
-    "timecc": "Time spent with crown control per min.",
-    "totdmgtaken": "Total damage taken per min.",
-    "magicdmgtaken": "Magic damage taken per min.",
-    "physdmgtaken": "Physical damage taken per min.",
-    "truedmgtaken": "True damage taken per min.",
-    "goldearned": "Gold earned per min.",
-    "goldspent": "Gold spent per min.",
-    "totminionskilled": "Total minions killed per min.",
-    "neutralminionskilled": "Neutral minions killed per min.",
-    "ownjunglekills": "Own jungle kills per min.",
-    "enemyjunglekills": "Enemy jungle kills per min.",
-    "totcctimedealt": "Total crown control time dealt per min.",
-    "pinksbought": "Pink wards bought per min.",
-    "wardsbought": "Wards bought per min.",
-    "wardsplaced": "Wards placed per min.",
-    "turretkills": "# of turret kills",
-    "inhibkills": "# of inhibitor kills",
-    "dmgtoturrets": "Damage to turrets"
-}
-feature_names = [full_names.get(n, n) for n in X.columns]
-X.columns = feature_names
-
-# create train/validation split
-Xt, Xv, yt, yv = train_test_split(X,y, test_size=0.2, random_state=10)
-dt = xgb.DMatrix(Xt, label=yt.values)
-dv = xgb.DMatrix(Xv, label=yv.values)
 
